50046e91233f4e22401972442fcfa4b233a2fade
Alain Lavanchy <alain.lavanchy@gmail.com>
2023-09-29 21:01:09 +0200

    Testlog added, unittest for successful open

--- read_logfile_test.py
+++ read_logfile_test.py
@@ -13,6 +13,14 @@ class TestOpenLogFile(unittest.TestCase):
             read_logfile(testcase)
         self.assertEqual(cm.exception.code, 1)
 
+    def test_logfile_exists(self):
+        testcase = "testlog.log"
+        expected = "Sep 26 14:25:10 my-server-01 kernel: WARNING: CPU temperature is above safe threshold [12345]\n"
+        file = read_logfile(testcase)
+        teststring = file.readline()
+        file.close()
+        self.assertEqual(teststring,expected)
+
     def test_filname_is_no_string(self):
         testcase = [0,1]
         with self.assertRaises(SystemExit) as dm:
