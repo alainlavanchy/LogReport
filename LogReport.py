60009d1335162f1b0ea0c79a70f5bbe4a70f4f1e
Alain Lavanchy <alain.lavanchy@gmail.com>
2023-10-01 23:03:01 +0200

    Created the reporting functions and csv

--- LogReport.py
+++ LogReport.py
@@ -2,18 +2,20 @@
 """LogReport Generator
 
 This script takes a log file as input and generates two reports from the logs.
--> An error log
--> A usage log, showing how often each user has used the service.
+-> An error log, showing all the error messages sorted by their frequency.
+-> A usage log, showing how often each user has used the service and how many errors he forced.
 
 Used modules:
 -> re - to search for the entries in the logfile using regex
 -> logging - to log errors
 -> os - handle the files
+-> csv - to create the csv files
 """
 
 import re
 import logging
 import os
+import csv
 
 
 
@@ -85,7 +87,7 @@ def read_usage(file_object):
 def create_error_report(error_logs):
     """
     Parameters:
-    error_logs (list): List containing all information on the errors in tuples.
+    error_logs (list): List containing all information on the errors, in tuples.
 
     Returns:
     error_report (dict): Dictionary containing the statistics of the errors found in the log.
@@ -93,60 +95,127 @@ def create_error_report(error_logs):
     The function generates a dictionary listing the error causes and counts them. Sorted by counts.
     """
     error_report = {}
-    return error_report
+    for errors in error_logs:
+        error_message = errors[1][1:-1]
+        if error_message in error_report:
+            error_report[error_message] += 1
+        else:
+            error_report[error_message] = 1
+    myKeys = list(error_report.keys())
+    myKeys.sort()
+    error_report_sorted = {i: error_report[i] for i in myKeys}
+    logging.info(error_report_sorted)
+    return error_report_sorted
 
-def create_usage_report(usage_logs):
+def create_usage_error_report(usage_logs, error_logs):
     """
     Parameters:
-    file_object (file): File object containing the log file
+    usage_logs (list): List containing all information on the usage, in tuples.
+    error_logs (list): List containing all information on the errors, in tuples
 
     Returns:
-    usage_log (list): The found usages in the log as a list of tuples.
+    usage_report (dict): Dictionary containing the statistics of the usage information found in the log.
 
-    The function extracts all usage info marked with INFO:. The generates a list of tuples containing the INFO mark, the actual usage and the username of the user assigend to the action.
+    The function generates a dictionary listing the usage of the script by user. Sorted by counts.
     """
     usage_report = {}
-    return usage_report
+    for usage in usage_logs:
+        username = usage[2]
+        username = username[1:-1]
+        if username in usage_report:
+            usage_report[username]["usage"] += 1
+        else:
+            usage_report[username] = {}
+            usage_report[username]["usage"] = 1
+            usage_report[username]["errors"] = 0
+    for errors in error_logs:
+        username = errors[2]
+        username = username[1:-1]
+        if username in usage_report:
+            if "errors" in usage_report[username]:
+                usage_report[username]["errors"] += 1
+            else:
+                usage_report[username]["errors"] = 1
+        else:
+            usage_report[username] = {}
+            usage_report[username]["errors"] = 1
+            usage_report[username]["usage"] = 0
+    myKeys = list(usage_report.keys())
+    myKeys.sort()
+    usage_report_sorted = {i: usage_report[i] for i in myKeys}
+    logging.info(usage_report_sorted)
+    return usage_report_sorted
 
 
-def write_usage_csv(usage_logs, usage_csv_file):
+def write_usage_csv(usage_report, usage_csv_file):
     """
     Parameters:
-    file_object (file): File object containing the log file
+    usage_report (dict): Dictonary containg the information on the usage of the script
+    usage_csv_file (str): The filename of the csv file to be created.
 
     Returns:
-    usage_log (list): The found usages in the log as a list of tuples.
+    Nothing
 
-    The function extracts all usage info marked with INFO:. The generates a list of tuples containing the INFO mark, the actual usage and the username of the user assigend to the action.
+    The function creates a csv file, using the information in the dictionary.
     """
-    if not isinstance(usage_logs, list):
-        logging.error('Usage log has to be a list.')
+    if not isinstance(usage_report, dict):
+        logging.error('Usage report has to be a dictionary.')
         exit(1)
     if not isinstance(usage_csv_file, str):
         logging.error('CSV filename has to be a string.')
         exit(1)
-    usage_freq = {}
-    for usage in usage_logs:
-        if usage[3] in usage_freq:
-            usage_freq[usage[3]] += 1
-        else:
-            usage_freq[usage[3]] = 1
-    print(usage_freq)
+
+    #Defining the fields for the csv columns
+    fields = ["Username", "Usage", "Errors"]
+
+    #Opening the csv file
+    with open (usage_csv_file, "w", newline="") as csv_file:
+        #Create the csv writer
+        writer = csv.DictWriter(csv_file, fieldnames=fields)
+        writer.writeheader()
+        usernames = list(usage_report.keys())
+        for username in usernames:
+            line = {}
+            line["Username"] = username
+            line["Errors"] = usage_report[username]["errors"]
+            line["Usage"] = usage_report[username]["usage"]
+            writer.writerow(line)
+    logging.info("CSV Report {} created".format(usage_csv_file))
     return
 
-def write_error_csv(error_logs, error_csv_file):
+def write_error_csv(error_report, error_csv_file):
     """
     Parameters:
-    file_object (file): File object containing the log file
+    error_report (dict): Dictonary containg the information on the errors of the script
+    error_csv_file (str): The filename of the csv file to be created.
 
     Returns:
-    usage_log (list): The found usages in the log as a list of tuples.
+    Nothing
 
-    The function extracts all usage info marked with INFO:. The generates a list of tuples containing the INFO mark, the actual usage and the username of the user assigend to the action.
+    The function creates a csv file, using the information in the dictionary.
     """
-    if not isinstance(error_logs, list):
-        logging.error('Usage log has to be a list.')
+    if not isinstance(error_report, dict):
+        logging.error('Error report has to be a dictionary.')
         exit(1)
+
+    if not isinstance(error_csv_file, str):
+        logging.error('CSV filename has to be a string.')
+        exit(1)
+    #Defining the fields for the csv columns
+    fields = ["Error", "Counts"]
+
+    #Opening the csv file
+    with open (error_csv_file, "w", newline="") as csv_file:
+        #Create the csv writer
+        writer = csv.DictWriter(csv_file, fieldnames=fields)
+        writer.writeheader()
+        errors = list(error_report.keys())
+        for error in errors:
+            line = {}
+            line["Error"] = error
+            line["Counts"] = error_report[error]
+            writer.writerow(line)
+    logging.info("CSV Report {} created".format(error_csv_file))
     return
 
 def main():
@@ -158,8 +227,12 @@ def main():
     file_object2 = read_logfile(file)
     find_usage = read_usage(file_object)
     find_errors = read_errors(file_object2)
+    usage_report = create_usage_error_report(find_usage, find_errors)
+    error_report = create_error_report(find_errors)
     file_object.close()
     file_object2.close()
+    write_error_csv(error_report, "error_report.csv")
+    write_usage_csv(usage_report, "usage_report.csv")
     logging.info('End Logging')
 
 if __name__ == "__main__":
